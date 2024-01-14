96cbfb96ce2845d468d774e11688f6638d8b2e6c
SD <53436688+Soumyajit74@users.noreply.github.com>
2024-01-15 04:22:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ scissors = '''
 '''
 game_images = [rock, paper, scissors]
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if user_choice >= 3 or user_choice < 0: 
+human_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if human_choice >= 3 or human_choice < 0: 
     print("You typed an invalid number, you lose!") 
 else:
     print(game_images[user_choice])
@@ -39,13 +39,13 @@ else:
     print(game_images[computer_choice])
 
 
-    if user_choice == 0 and computer_choice == 2:
+    if human_choice == 0 and computer_choice == 2:
         print("You win!")
-    elif computer_choice == 0 and user_choice == 2:
+    elif computer_choice == 0 and human_choice == 2:
         print("You lose")
-    elif computer_choice > user_choice:
+    elif computer_choice > human_choice:
         print("You lose")
-    elif user_choice > computer_choice:
+    elif human_choice > computer_choice:
         print("You win!")
-    elif computer_choice == user_choice:
+    elif computer_choice == human_choice:
         print("It's a draw")
